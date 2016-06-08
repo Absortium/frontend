620007eb758ac00c0ede1dda78785481df9d0d58
Andrey Samokhvalov <andrew.shvv@gmail.com>
2016-06-08 12:54:23 +0300

    first exchangebox calculation tests

--- app/containers/ExchangeBox/tests/reducer.test.js
+++ app/containers/ExchangeBox/tests/reducer.test.js
@@ -1,7 +1,22 @@
-import expect from "expect";
+import {expect} from "chai";
+import {shallow} from 'enzyme';
+import React from 'react';
 import exchageBoxReducer from "../reducer";
 import { fromJS } from "immutable";
+import {
+    changeFromAmount,
+    changeRate,
+    changeToAmount
+} from "../actions"
 
+import ExchangeBox from "../index"
+
+import {
+    loggedIn,
+    accountsReceived,
+    marketChanged,
+    marketInfoReceived
+} from "../../App/actions";
 
 const initialState = {
     isAuthenticated: false,
@@ -16,11 +31,120 @@ const initialState = {
     to_currency: null,
 };
 
+var accounts = {
+    btc: {
+        address: "wxaH5tcgFYLYwJiyPmMNFgSLFyKKMP",
+        amount: 400000000,
+        pk: 2
+    }
+};
+
+var marketInfo = {
+    btc: {
+        eth: {
+            rate: "0.00000000",
+            rate_24h_max: "0.00000000",
+            rate_24h_min: "0.00000000",
+            volume_24h: 0
+        }
+    }
+};
+
 describe('ExchangeBoxReducer', () => {
     it('returns the initial state', () => {
-        expect(exchageBoxReducer(undefined, {})).toEqual(initialState);
+        expect(exchageBoxReducer(undefined, {})).to.deep.equal(initialState);
     });
-    it('returns the initial state', () => {
-        expect(exchageBoxReducer(undefined, {})).toEqual(initialState);
+
+    it('change market', () => {
+        let state = exchageBoxReducer({}, marketChanged('btc', 'eth'));
+
+        expect(state).to.deep.equal({
+            from_currency: 'btc',
+            to_currency: 'eth'
+        });
     });
+
+    it('is authenticated', () => {
+        let state = exchageBoxReducer({}, loggedIn());
+
+        expect(state).to.deep.equal({ isAuthenticated: true });
+    });
+
+    it('accounts loaded', () => {
+        let expectedState = {};
+
+        let state = exchageBoxReducer({}, marketChanged('btc', 'eth'));
+        expectedState['from_currency'] = 'btc';
+        expectedState['to_currency'] = 'eth';
+
+        state = exchageBoxReducer(state, loggedIn());
+        expectedState['isAuthenticated'] = true;
+
+        state = exchageBoxReducer(state, accountsReceived(accounts));
+        expectedState['account'] = accounts['btc'];
+        expectedState['isAccountLoaded'] = true;
+        expectedState['isAccountExist'] = true;
+        expectedState['from_amount'] = 400000000;
+
+        expect(state).to.deep.equal(expectedState);
+    });
+
+    it('market info received', () => {
+        let expectedState = {};
+
+        let state = exchageBoxReducer({}, marketChanged('btc', 'eth'));
+        expectedState['from_currency'] = 'btc';
+        expectedState['to_currency'] = 'eth';
+
+        state = exchageBoxReducer(state, loggedIn());
+        expectedState['isAuthenticated'] = true;
+
+        state = exchageBoxReducer(state, accountsReceived(accounts));
+        expectedState['account'] = accounts['btc'];
+        expectedState['isAccountLoaded'] = true;
+        expectedState['isAccountExist'] = true;
+        expectedState['from_amount'] = 400000000;
+
+        state = exchageBoxReducer(state, marketInfoReceived(marketInfo));
+        expectedState['isRateLoaded'] = true;
+        expectedState['rate'] = 0;
+        expectedState['to_amount'] = 0;
+
+        expect(state).to.deep.equal(expectedState);
+    });
+
+    // it('calculation #1', () => {
+    //     let [state, expectedState] = preinit();
+    //
+    //     state = exchageBoxReducer(state, accountsReceived(accounts));
+    //     expectedState['account'] = accounts['btc'];
+    //     expectedState['isAccountLoaded'] = true;
+    //     expectedState['isAccountExist'] = true;
+    //     expectedState['from_amount'] = 400000000;
+    //
+    //     const wrapper = shallow(<ExchangeBox />);
+    //     console.log(wrapper);
+    //
+    // });
 });
+
+function preinit() {
+    let expectedState = {};
+
+    let state = exchageBoxReducer({}, marketChanged('btc', 'eth'));
+    expectedState['from_currency'] = 'btc';
+    expectedState['to_currency'] = 'eth';
+
+    state = exchageBoxReducer(state, loggedIn());
+    expectedState['isAuthenticated'] = true;
+
+    state = exchageBoxReducer(state, marketInfoReceived(marketInfo));
+    expectedState['isRateLoaded'] = true;
+    expectedState['rate'] = 0;
+    expectedState['to_amount'] = 0;
+
+    return [state, expectedState]
+}
+function pprint(obj) {
+    console.log(JSON.stringify(obj, null, 2));
+}
