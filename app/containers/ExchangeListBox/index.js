cbd39d59ffb29c39b988fee6fb8fb19a5a338dee
Andrey Samokhvalov <andrew.shvv@gmail.com>
2016-06-19 18:24:25 +0300

    Fix strange behavior of ExchangeListBox

--- app/containers/ExchangeListBox/index.js
+++ app/containers/ExchangeListBox/index.js
@@ -8,24 +8,15 @@ import React from "react";
 import { connect } from "react-redux";
 import selectExchangeListBox from "./selectors";
 import Refresh from "components/Refresh";
-import StatusIcon from "components/StatusIcon";
+import AllExchangeTable from "components/AllExchangeTable";
+import UserExchangeTable from "components/UserExchangeTable";
 import Paper from "material-ui/Paper";
+import Subheader from "material-ui/Subheader"
 import {
     Tabs,
     Tab
 } from "material-ui/Tabs";
-import {
-    Table,
-    TableBody,
-    TableFooter,
-    TableHeader,
-    TableHeaderColumn,
-    TableRow,
-    TableRowColumn
-} from "material-ui/Table";
 import ExchangeTabLabel from "components/ExchangeTabLabel";
-import { normalize } from "utils/general";
-import BigNumber from "bignumber.js";
 
 /**
  *
@@ -68,106 +59,46 @@ const styles = {
 class ExchangeListBox extends React.Component {
     render() {
         return (
-            <div>
-
-                <Paper style={styles.block} zDepth={2}>
+            <Paper style={styles.block} zDepth={2}>
+                { this.props.isAuthenticated ?
                     <Tabs>
                         <Tab label={<ExchangeTabLabel text="All Exchanges"/>}
                              style={styles.tab}>
                             { this.props.isAllExchangesLoaded ?
-                                <Table
-                                    height="18.6em"
-                                    fixedHeader={true}
-                                    selectable={true}
-                                    multiSelectable={false}>
-                                    <TableHeader
-                                        displaySelectAll={false}
-                                        adjustForCheckbox={false}
-                                        enableSelectAll={false}>
-                                        <TableRow>
-                                            <TableHeaderColumn>{"Give (" + this.props.from_currency.toUpperCase() + ")"}</TableHeaderColumn>
-                                            <TableHeaderColumn>{"Price (" + this.props.to_currency.toUpperCase() + ")"}</TableHeaderColumn>
-                                            <TableHeaderColumn>{"Get (" + this.props.to_currency.toUpperCase() + ")"}</TableHeaderColumn>
-                                        </TableRow>
-                                    </TableHeader>
-                                    <TableBody
-                                        displayRowCheckbox={false}
-                                        deselectOnClickaway={true}
-                                        showRowHover={true}
-                                        stripedRows={false}>
-                                        {this.props.all_exchanges.map((row, index) => {
-                                            let price = new BigNumber(row.price);
-                                            let amount = new BigNumber(row.amount);
-
-                                            return (
-                                                <TableRow key={index}>
-
-
-                                                    <TableRowColumn>{normalize(amount)}</TableRowColumn>
-                                                    <TableRowColumn>{normalize(price)}</TableRowColumn>
-                                                    <TableRowColumn>{normalize(price.times(amount))}</TableRowColumn>
-                                                </TableRow>
-                                            )}
-                                        )}
-                                    </TableBody>
-                                </Table>
+                                <AllExchangeTable exchanges={this.props.all_exchanges}
+                                                  from_currency={this.props.from_currency}
+                                                  to_currency={this.props.to_currency}/>
                                 :
                                 <Refresh />
                             }
                         </Tab>
-                        { this.props.isAuthenticated ?
-                            <Tab label={<ExchangeTabLabel text="User Exchanges"/>}
-                                 style={styles.tab}>
-                                { this.props.isUserExchangesLoaded ?
-                                    <Table
-                                        height="18.6em"
-                                        fixedHeader={true}
-                                        selectable={true}
-                                        multiSelectable={false}>
-                                        <TableHeader
-                                            displaySelectAll={false}
-                                            adjustForCheckbox={false}
-                                            enableSelectAll={false}>
-                                            <TableRow>
-                                                <TableHeaderColumn>{"Give (" + this.props.from_currency.toUpperCase() + ")"}</TableHeaderColumn>
-                                                <TableHeaderColumn>{"Price (" + this.props.to_currency.toUpperCase() + ")"}</TableHeaderColumn>
-                                                <TableHeaderColumn>{"Get (" + this.props.to_currency.toUpperCase() + ")"}</TableHeaderColumn>
-                                                <TableHeaderColumn>Status</TableHeaderColumn>
-                                            </TableRow>
-                                        </TableHeader>
-                                        <TableBody
-                                            displayRowCheckbox={false}
-                                            deselectOnClickaway={true}
-                                            showRowHover={true}
-                                            stripedRows={false}>
-                                            {this.props.user_exchanges.map((row, index) => {
 
-                                                let price = new BigNumber(row.price);
-                                                let amount = new BigNumber(row.amount);
-
-                                                return (
-                                                    <TableRow key={index}>
-                                                        <TableRowColumn>{normalize(amount)}</TableRowColumn>
-                                                        <TableRowColumn>{normalize(price)}</TableRowColumn>
-                                                        <TableRowColumn>{normalize(amount.times(price))}</TableRowColumn>
-                                                        <TableRowColumn><StatusIcon
-                                                            status={row.status}/></TableRowColumn>
-                                                    </TableRow>
-                                                )
-                                            })
-                                            }
-                                        </TableBody>
-                                    </Table>
-                                    :
-                                    <Refresh />
-                                }
-
-                            </Tab>
-                            :
-                            null}
+                        <Tab label={<ExchangeTabLabel text="User Exchanges"/>}
+                             style={styles.tab}>
+                            { this.props.isUserExchangesLoaded ?
+                                <UserExchangeTable exchanges={this.props.user_exchanges}
+                                                   from_currency={this.props.from_currency}
+                                                   to_currency={this.props.to_currency}/>
+                                :
+                                <Refresh />
+                            }
+                        </Tab>
                     </Tabs>
-                </Paper>
-            </div>
+                    :
+                    <div>
+                        <Subheader style={styles.subheader}>
+                            All Exchanges
+                        </Subheader>
+                        { this.props.isAllExchangesLoaded ?
+                            <AllExchangeTable exchanges={this.props.all_exchanges}
+                                              from_currency={this.props.from_currency}
+                                              to_currency={this.props.to_currency}/>
+                            :
+                            <Refresh />
+                        }
+                    </div>
+                }
+            </Paper>
         )
     }
 }
